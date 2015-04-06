8d811a58f7a33c87e4ee2722a63d5540aedc3607
jaishekar <jaishekarjai@gmail.com>
2015-04-06 00:21:30 -0700

    slide up and down

--- js/DigiJs.js
+++ js/DigiJs.js
@@ -1,5 +1,6 @@
 // on load
 	$(document).ready(function(){ 
+	$('body').css('overflow-y', 'hidden');
 	//hide the sidebar wrapper //
 		$("#sidebar-wrapper").hide();
 	// on load hide all the other sections except the landing page //
@@ -9,7 +10,136 @@
 		$("#blog").hide();
 		$("#contact").hide();
 		$("#clients").hide();
+		setTabs();
+		
+		$("#top").focus();
+		
 	});
+	
+	function setTabs(){
+		
+		$('#top').attr("tabindex",1);
+		$("#services").attr("tabindex",2);
+		$("#about").attr("tabindex",3);
+		$("#portfolio").attr("tabindex",4);
+		$("#blog").attr("tabindex",5);
+		$("#contact").attr("tabindex",6);
+		$("#clients").attr("tabindex",7);
+		
+	}
+	$('#top').keydown(function(event){
+		
+		if(event.which == 40)
+		{
+			$('#top').slideUp(1000);
+			$('#top').blur();
+			
+			$('#services').show();
+			$("#about").hide();
+			$("#portfolio").hide();
+			$("#blog").hide();
+			$("#contact").hide();
+			
+			
+			$('#services').focus();
+		
+		}
+	});
+
+	$('#services').keydown(function(event){
+		
+		if(event.which == 38) 
+		{
+			 
+			 
+			$('#services').slideDown("1000");
+			$('#services').blur();
+			$('#top').show();
+			$("#about").hide();
+			$("#portfolio").hide();
+			$("#blog").hide();
+			$("#contact").hide();
+			$("#clients").hide();
+	
+			$('#top').focus();
+		}
+		else if(event.which == 40) 
+		{
+			
+			$('#services').slideUp("1000");
+			$('#services').blur();
+			$('#about').show();
+			
+			$("#portfolio").hide();
+			$("#blog").hide();
+			$("#contact").hide();
+			$("#clients").hide();
+		
+			$('#about').focus();
+		}
+	});
+	$('#about').keydown(function(event){
+		switch(event.which)
+		{
+			case 38:
+					
+					$('#about').slideDown("1000");
+					$('#about').blur();
+					$('#services').show();
+					$("#portfolio").hide();
+					$("#top").hide();
+					$("#blog").hide();
+					$("#contact").hide();
+					$("#clients").hide();
+				
+					$('#services').focus();
+					break;
+			case 40:
+					$('#about').slideUp("1000");
+					$('#about').blur();
+					$('#portfolio').show();
+				
+					$('#portfolio').focus();
+					break;
+			
+		}
+		
+		
+		
+	});
+	$('#portfolio').keydown(function(event){
+		switch(event.which)
+		{
+			case 38:
+					$('#portfolio').slideDown("1000");
+					$("#portfolio").blur();
+					$('#about').show();
+				
+					$('#about').focus();
+					break;
+			case 40:
+					$('#portfolio').slideUp("1000");
+					$("#portfolio").blur();
+					$('#contact').show();
+				
+					$('#contact').focus();
+					break;
+		}
+	});
+	$('#contact').keydown(function(event){
+		if(event.which==38)
+		{
+			
+				$('#contact').slideDown("1000");
+				$("#contact").blur();
+				$('#portfolio').show();
+			
+				$('#portfolio').focus();
+				
+			
+		}
+	});
+	
     // Closes the sidebar menu
     $("#menu-close").click(function(e) {
         e.preventDefault();
@@ -29,6 +159,7 @@
 		if(menu_href=="#top"){
 			$("#sidebar-wrapper").slideUp("slow");
 			$("#top").show();
+			$("#top").focus();
 			$("#services").hide();
 			$("#about").hide();
 			$("#portfolio").hide();
@@ -38,6 +169,7 @@
 		else if(menu_href=="#services"){
 			$("#sidebar-wrapper").slideUp("slow");
 			$("#services").show();
+			$("#services").focus();
 			$("#top").hide();
 			$("#about").hide();
 			$("#portfolio").hide();
@@ -49,6 +181,7 @@
 			$("#top").hide();
 			$("#services").hide();
 			$("#about").show();
+			$("#about").focus();
 			$("#portfolio").hide();
 			$("#blog").hide();
 			$("#contact").hide();
@@ -59,6 +192,7 @@
 			$("#services").hide();
 			$("#about").hide();
 			$("#portfolio").show();
+			$("#portfolio").focus();
 			$("#blog").hide();
 			$("#contact").hide();
 		}
@@ -69,6 +203,7 @@
 			$("#about").hide();
 			$("#portfolio").hide();
 			$("#blog").show();
+			$("#blog").focus();
 			$("#contact").hide();
 		}
 		else if(menu_href=="#contact"){
@@ -79,6 +214,7 @@
 			$("#portfolio").hide();
 			$("#blog").hide();
 			$("#contact").show();
+			$("#contact").focus();
 		}
 	});
 	
